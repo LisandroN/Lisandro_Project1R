91179f2356026d2964926f0406e07413cad756b1
Lisandro Neris <lisandroneris07@gmail.com>
2021-12-08 22:34:12 -0500

    last commit after deploy

--- src/App.js
+++ src/App.js
@@ -8,29 +8,28 @@ import Project1 from './Project1';
 import Project2 from './Project2';
 import './App.css';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
+  
   return (
-    <div className = "App">
+    
+    <div className="App">
 
-    <Router>
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/home" element={<Home/>}/>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
 
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/project1" element={<Project1/>}/>
-        <Route path="/project2" element={<Project2/>}/>
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project1" element={<Project1 />} />
+            <Route path="/project2" element={<Project2 />} />
 
-
-
-
-      </Routes>
-  </Router>
-    </div>
+          </Routes>
+        </Router>
+      </div></></>
   );
 }
 
